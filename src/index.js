3a018c233c78421b7240c645f665e3c51690daf6
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-11-20 08:12:19 +0200

    20.11.2022 08:12

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+// import 'modern-normalize/modern-normalize.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
