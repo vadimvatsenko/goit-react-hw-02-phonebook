27dacc71745b8c1f672052b501967def9917e442
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-11-22 14:01:32 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -60,7 +60,7 @@ export class App extends Component {
   };
 
   render() {
-    const { contacts, filter } = this.state;
+    const { filter } = this.state;
     const visibleContacts = this.getVisibleContatcts() 
 
   
